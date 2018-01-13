140230f7287de8c0e0474e5d811056047caf8925
mistermeme <matrix0222@gmail.com>
2018-01-13 11:27:48 -0500

    # WARNING: head commit changed in the meantime
    bwee

--- talontimecharles/src/org/usfirst/frc/team610/robot/Robot.java
+++ talontimecharles/src/org/usfirst/frc/team610/robot/Robot.java
@@ -1,11 +1,10 @@
 package org.usfirst.frc.team610.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
+import edu.wpi.first.wpilibj.Joystick;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -15,20 +14,43 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * directory.
  */
 public class Robot extends IterativeRobot {
+	TalonSRX leftIntake, rightIntake, rightDrive, leftDrive;
+	Joystick driver;
+	boolean pressed;
 	
-	TalonSRX talonOne;
-	TalonSRX talonTwo;
+	double leftIntakeSpeed = 0, rightIntakeSpeed = 0;
 
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
 	@Override
 	public void robotInit() {
-		talonOne = new TalonSRX(1);
-		talonTwo = new TalonSRX(2);
+		driver       = new Joystick(0);
+		leftIntake   = new TalonSRX(1);
+		rightIntake  = new TalonSRX(2);
+		rightDrive   = new TalonSRX(3);
+		leftDrive    = new TalonSRX(4);
+				
+		leftDrive.set(ControlMode.PercentOutput, 0);
+		rightDrive.set(ControlMode.PercentOutput, 0);
+		leftIntake.set(ControlMode.PercentOutput, 0);
+		rightIntake.set(ControlMode.PercentOutput, 0);
 	}
-
-	
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the
+	 * switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
 	@Override
 	public void autonomousInit() {
-	
+		
 	}
 
 	/**
@@ -36,7 +58,7 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousPeriodic() {
-
+		
 	}
 
 	/**
@@ -44,6 +66,22 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
+		double x = driver.getRawAxis(2);
+		double y = driver.getRawAxis(1);
+		if (driver.getRawButton(8)) {
+			leftIntakeSpeed = driver.getRawAxis(1);
+			rightIntakeSpeed = driver.getRawAxis(3);
+			x = 0; y=0;
+		}		
+		
+		double leftSpeed = -(x + y);
+		double rightSpeed = -(x - y);
+		leftIntake.set(ControlMode.PercentOutput, leftIntakeSpeed);
+		rightIntake.set(ControlMode.PercentOutput, rightIntakeSpeed);
+		
+		leftDrive.set(ControlMode.PercentOutput, leftSpeed);
+		rightDrive.set(ControlMode.PercentOutput, rightSpeed);
+
 		
 	}
 
@@ -52,6 +90,18 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void testPeriodic() {
+		leftDrive.set(ControlMode.PercentOutput, 1);
+		rightDrive.set(ControlMode.PercentOutput, 1);
+		leftIntake.set(ControlMode.PercentOutput, 1);
+		rightIntake.set(ControlMode.PercentOutput, 1);
+	}
+	
+	@Override
+	public void disabledPeriodic(){
+		leftDrive.set(ControlMode.PercentOutput, 1);
+		rightDrive.set(ControlMode.PercentOutput, 1);
+		leftIntake.set(ControlMode.PercentOutput, 1);
+		rightIntake.set(ControlMode.PercentOutput, 1);
 	}
 }
 
